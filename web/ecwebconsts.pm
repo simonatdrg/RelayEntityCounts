a8d99b62baf5c950972cec51d1df069811c9a35e
svr0002 <srosenthal@relaytm.com>
2015-06-16 12:35:02 -0400

    modified ec web app for Solr save httpd conf files here

--- web/ecwebconsts.pm
+++ web/ecwebconsts.pm
@@ -9,18 +9,19 @@ use strict;
 use Env qw ($HOME);
 #use Relay::Common qw ($RELAY_DATA);
 use Exporter ();
-use vars qw( $ECBASE $VERSION $ECTEMPDIR $ECSCRIPT $ATTIVIOURL @EXPORT); 
+use vars qw( $ECBASE $VERSION $ECTEMPDIR $ECSCRIPT $SOLRURL @EXPORT); 
 	
-$VERSION   = '1.0_02';
-our @ISA       = qw(Exporter);
-@EXPORT    = qw($ECBASE $ECSCRIPT $ECTEMPDIR $ATTIVIOURL);
+$VERSION   = '1.0_03';
+our @ISA   = qw(Exporter);
+@EXPORT    = qw($ECBASE $ECSCRIPT $ECTEMPDIR $SOLRURL);
 
 # change if nedded
 $ECTEMPDIR = "/data/temp/ec";
 # fix this 
 $ECSCRIPT = "${HOME}/relay-analysis/perl/ec.d/ec.pl";
 # this is hardwired for a partiuclar instance of a webapp, but could change (e.g. localhost)
-$ATTIVIOURL = "http://localhost:17001";
+$SOLRURL = "http://localhost:8983/solr/relaycontent/";
+#$ATTIVIOURL = "http://localhost:17001";
 # ecbase - base url which should reflect the environment (proxied by apache)
 $ECBASE = "http://localhost:3000/ec";
 #$ECBASE = "http://localhost:3000";
